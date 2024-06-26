dadb1e1f6d334bd625c5845c6ffce7fc21e78534
Yosuke Hatakeyama <yhr09749@gmail.com>
2024-06-26 09:38:00 +0900

    Fix.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  content: ['docs/index.html']
+  content: ['docs/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#53C8D2',
+        primaryLight: {
+          100: '#F0FEFF', // 90% 薄くした色
+          200: '#D8FBFF', // 80% 薄くした色
+          300: '#B7EFF5', // 60% 薄くした色
+          400: '#95E2EA', // 40% 薄くした色
+          500: '#74D5DE', // 20% 薄くした色
+        },
+      },
+    },
+  },
+  plugins: [],
 }
