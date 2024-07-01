4e944b1e2b10f35dace476133e1a3af81628edb9
Yosuke Hatakeyama <yhr09749@gmail.com>
2024-07-01 15:17:20 +0900

    Fix.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Noto Sans JP', 'sans-serif'],
-        poppins: ['Poppins', 'Noto Sans JP', 'sans-serif'],
+        jp: ['Noto Sans JP', 'sans-serif'],
+        en: ['Poppins', 'Noto Sans JP', 'sans-serif'],
       },
       colors: {
         primary: '#53C8D2',
